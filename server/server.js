943686c4f3d0fbfea025b7b4ce42c96ff844b394
Andrew Chung <aandrewchung@gmail.com>
2024-03-23 03:13:39 -0400

    added server route for getting users

--- server/server.js
+++ server/server.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const cors = require('cors');
 
+const { getUsers } = require('./utils/getUsers'); // Update the path according to your project structure
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Server is running');
+});
+
+app.get('/users', async (req, res) => {
+    try {
+      const users = await getUsers();
+      res.json(users);
+    } catch (error) {
+      console.error("Failed to get users", error);
+      res.status(500).send("Error retrieving users from the database.");
+    }
 });
 
 if (require.main === module) {
